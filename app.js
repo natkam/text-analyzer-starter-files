11ccb0dc12c1aea8ff2b55539dc34a87761fde6d
natka <natka_m@o2.pl>
2017-11-27 20:36:41 +0100

    Clean up the code, improve functionality and correct minor mistakes.
    Change the event type from 'click' to 'submit' (prevents an empty form from
    being passed to the handler), add event handler for 'reset' button, refactor
    the code and make it clearer.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
-/* If there are non-word-chars at the end of the text, slice() produces
-an additional empty string item at the end of the textArray. This funcion trims
+/* If there are non-word-chars at the end of the text, split() produces
+an additional empty string item at the end of the textArray. This function trims
 non-word-chars from the beginning and end of the text. */
 function trimNonLetters(txt) {
 	var start = txt.search(/\w+/);
@@ -11,35 +11,36 @@ function trimNonLetters(txt) {
 	return trimmedTxt;
 }
 
-function getTextStats() {
-	var txt = $('.js-text').val();
-  var textArray = txt //trimNonLetters(txt)
+function getTextStats(txt) {
+  var textArray = txt
 		.toLowerCase()
     .split(/[^\w-']+/); // this can't handle Polish letters though...
 		// .split(/[\\\/.,;!?"()\[\]&%#*@+=\s]+/); // yuck!
 	var wordCount = textArray.length;
+  var letterCount = 0;
 	var uniqueWordArray = [];
-	var letterCount = 0;
 	for (var i = 0, word; word = textArray[i]; i++) {
 		letterCount += word.length;
 		if (!uniqueWordArray.includes(word)) {
 			uniqueWordArray.push(word);
 		}
 	}
-	var uniqueWordCount = uniqueWordArray.length;
-	var averageWordLength = (letterCount/wordCount).toFixed(2);
-
-  $('.js-results').removeClass('hidden');
   $('.js-word-count').text(wordCount);
-  $('.js-unique-word-count').text(uniqueWordCount);
-  $('.js-average-word-length').text(averageWordLength);
+  $('.js-unique-word-count').text(uniqueWordArray.length);
+  $('.js-average-word-length').text((letterCount/wordCount).toFixed(2) + ' characters');
+  $('.js-results').removeClass('hidden');
 }
 
-function analyze() {
-  $('.js-analyze').on('click', function(event) {
+function handleForm() {
+  var textForm = $('.js-text-form');
+  textForm.submit(function(event) {
     event.preventDefault();
-    getTextStats();
+  	var txt = $(this).find('#user-text').val();
+    getTextStats(trimNonLetters(txt));
+  });
+  textForm.on('click', '.js-reset', function(){
+    $('.js-results').addClass('hidden');
   });
 }
 
-$(analyze);
+$(handleForm);
